6fe3dfa1b688f5ee7e7d972d8048e1030759e475
Erik Badger <erikbadger777@gmail.com>
2021-09-06 17:29:50 -0500

    feat: added compound interst calculation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,23 +20,38 @@ const InputHalf = styled.div`
   flex-direction: column;
   gap: 12rem;
 `
-const IndexPage = () => (
-  <Page>
-    <Layout>
-      <InputHalf>
-        <h1>How Much does your money cost?</h1>
-        <p>As we get older compound interest get’s less time to take effect. Use this tool to determine how much your money really costs</p>
-        <InputGroup>
-          <NumperInput placeholder="Spend" type="number" />
-          <NumperInput placeholder="Age" type="number" />
-        </InputGroup>
-      </InputHalf>
-      <InputHalf>
-        <h2>$108.89</h2>
-        <Sparkline width={500} height={385} />
-      </InputHalf>
-    </Layout>
-  </Page>
-)
+
+const calculateCompoundInterest = (p: number, interestRate: number, time: number) => Math.round(p * (1 + interestRate) ** time)
+const IndexPage = () => {
+  const blah = ''
+  const [age, setAge] = React.useState<number>()
+  const [principal, setPrincipal] = React.useState<number>()
+  const [end] = React.useState(50)
+  const [interestRate] = React.useState(0.07)
+  const total = React.useMemo(() => {
+    if (!principal || !age) return 0
+    return calculateCompoundInterest(principal, interestRate, end - age)
+  }, [age, principal, end, interestRate])
+  return (
+    <Page>
+      <Layout>
+        <InputHalf>
+          <h1>How Much does your money cost?</h1>
+          <p>
+            As we get older compound interest get’s less time to take effect. Use this tool to determine how much your money really costs
+          </p>
+          <InputGroup>
+            <NumperInput placeholder="Spend" type="number" onChange={(e) => setPrincipal(e.target.value)} />
+            <NumperInput placeholder="Age" type="number" onChange={(e) => setAge(e.target.value)} />
+          </InputGroup>
+        </InputHalf>
+        <InputHalf>
+          <h2>$ {total}</h2>
+          <Sparkline width={500} height={385} />
+        </InputHalf>
+      </Layout>
+    </Page>
+  )
+}
 
 export default IndexPage
